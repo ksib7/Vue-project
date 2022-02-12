e0f1e7da01cf09cb5f9db886391a89118cacc32d
ksib7 <sibirkin70@gmail.com>
2022-02-13 02:25:16 +0300

    vue project

---
+++ vue_test_project/src/components/UI/myButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button class="btn">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.btn {
+  font-size: 18px;
+  color: white;
+  background: none;
+  border: none;
+  background: teal;
+  padding: 10px;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  opacity: 0.6;
+}
+</style>
---
+++ vue_test_project/src/components/postForm.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <form @submit.prevent class="form" action="">
+      <input
+        class="inputEl"
+        type="text"
+        placeholder="Название"
+        required
+        v-model="post.title"
+      />
+      <input
+        class="inputEl"
+        type="text"
+        placeholder="Описание"
+        required
+        v-model="post.body"
+      />
+      <my-button class="btn" @click="createPost">Добавить</my-button>
+    </form>
+  </div>
+</template>
+
+<script>
+import myButton from "./UI/myButton";
+
+export default {
+  components: {
+    myButton,
+  },
+
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post);
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+}
+
+.inputEl {
+  width: 100%;
+  min-height: 70px;
+  border: 1px solid teal;
+  border-radius: 10px;
+  margin-top: 10px;
+  font-size: 18px;
+  padding: 10px;
+}
+
+.btn {
+  align-self: flex-end;
+  margin-top: 50px;
+}
+</style>
